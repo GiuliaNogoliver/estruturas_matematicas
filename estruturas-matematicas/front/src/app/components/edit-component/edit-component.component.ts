0edfc5189a6931d0445a1d66c3104061d964135a
Giulianogoliver <giulianogoliver84@gmail.com>
2024-05-11 15:11:39 -0300

    show image

--- estruturas-matematicas/front/src/app/components/edit-component/edit-component.component.ts
+++ estruturas-matematicas/front/src/app/components/edit-component/edit-component.component.ts
@@ -6,9 +6,11 @@ import { UploadService } from '../../upload.service';
   standalone: true,
   imports: [],
   templateUrl: './edit-component.component.html',
-  styleUrl: './edit-component.component.scss'
+  styleUrls: ['./edit-component.component.scss'] // Use styleUrls for styles
 })
 export class EditComponent {
+  imageUrl: string | null = null; // Initialize imageUrl to null
+  image: boolean = false;
 
   constructor(private uploadService: UploadService) { }
 
@@ -16,6 +18,13 @@ export class EditComponent {
     const file = event.target.files[0];
     const formData = new FormData();
     formData.append('file', file);
+
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = (e: any) => {
+      this.imageUrl = e.target.result as string;
+      this.image = true;
+    };
 
     this.uploadService.uploadFile(formData).subscribe(
       response => {
